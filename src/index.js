056dd1341e846bbe8208b7e975b45df1b3c29883
zhuo.zhang <zhuo.zhang@unspay.com>
2018-06-26 21:46:05 +0800

    增加redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SwitchPage from './SwitchPage';
-import {HashRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from "react-redux";
+import {ConnectedRouter, routerReducer, routerMiddleware} from 'react-router-redux';
+import createHistory from 'history/createHashHistory';
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(
+   combineReducers({
+      ...reducers,
+      router: routerReducer
+   }),
+   applyMiddleware(middleware));
+
 ReactDOM.render((
-   <HashRouter>
-      <SwitchPage/>
-   </HashRouter>
+   <Provider store={store}>
+      <ConnectedRouter history={history}>
+         <SwitchPage/>
+      </ConnectedRouter>
+   </Provider>
 ), document.getElementById('root'));
+
 registerServiceWorker();
