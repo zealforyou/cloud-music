fd93411d91c959833d32085beff74d32fa125740
huangtaining <taining.huang@unspay.com>
2018-07-06 16:35:52 +0800

    收藏页

---
+++ src/view/Collection.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+export default class Collection extends Component {
+   constructor() {
+      super();
+   }
+
+   //组件即将挂载
+   componentWillMount() {
+
+   }
+
+   //组件已经挂载
+   componentDidMount() {
+
+   }
+
+   //组件即将销毁
+   componentWillUnmount() {
+
+   }
+
+   //渲染
+   render() {
+      return (
+         <div>
+的谁更适合
+         </div>
+      )
+   }
+}
