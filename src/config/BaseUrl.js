90e2e3b0b36e41e8d2cb1096a65db091235b8fd6
zhangzhuo <916755664@qq.com>
2018-07-09 22:48:57 +0800

    Merge branch 'express' of github.com:zealforyou/cloud-music into express
    # Conflicts:
    #	src/RouterPage.js
    #	src/config/BaseUrl.js
    #	src/page/SearchPageIner.js

--- src/config/BaseUrl.js
+++ src/config/BaseUrl.js
@@ -1,5 +1,5 @@
 var BASE_URL={
-  base:"https://3329e16c.ngrok.io/",
-  // base:"http://192.168.2.214/",
+  // base:"https://3329e16c.ngrok.io/",
+  base:"http://192.168.2.214/",
 };
 module.exports=BASE_URL;
