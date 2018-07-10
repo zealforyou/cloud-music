7aca21b7f2cc4d2940d87fdf1722df7d2ce294d5
zealforyou <zhuo.zhang@unspay.com>
2018-07-10 18:18:35 +0800

    发布

--- src/config/BaseUrl.js
+++ src/config/BaseUrl.js
@@ -1,5 +1,5 @@
 var BASE_URL={
-  // base:"https://b9bc156e.ngrok.io/",
-  base:"http://172.22.200.101/",
+  base:"https://2b24bf96.ngrok.io/",
+  // base:"http://172.22.200.101/",
 };
 module.exports=BASE_URL;
