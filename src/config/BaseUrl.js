5dabd9457cf08a0794f6fa2248dc80a73bad5b45
zhangzhuo <916755664@qq.com>
2018-11-04 21:55:49 +0800

    mm

--- src/config/BaseUrl.js
+++ src/config/BaseUrl.js
@@ -1,5 +1,6 @@
 var BASE_URL={
   // base:"https://7c0f5161.ngrok.io/",
-  base:"http://172.22.200.101:80/",
+  // base:"http://172.22.200.101:80/",
+  base:"http://localhost:80/",
 };
 module.exports=BASE_URL;
